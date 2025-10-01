fa966ebbb6e44feb385dbb5cbc3b63bb6e817c69
Stilgarr <miso.ondrus@gmail.com>
2025-10-01 13:35:10 +0200

    Push

---
+++ Assets/BirdMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrenght;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrenght;
+        }
+        
+    }
+}
